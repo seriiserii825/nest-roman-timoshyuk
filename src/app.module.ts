302e1edcaf02abbb19217adddbccf4954349d65c
serii <seriiburduja@gmail.com>
2025-12-09 22:03:21 +0200

    feat: connect config and postgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,30 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+    CategoryModule,
+    AuthModule,
+    TransactionModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
---
+++ todo.md
@@ -0,0 +1,8 @@
+### todo
+
+<!-- - set postgres -->
+- set config from nest docs set global true
+- set .env variables
+- set db_name budget
+- install typeorm pg
+- set config in app.module.ts
