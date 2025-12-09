0efcfa95bfe3062423a54f250536066267745262
serii <seriiburduja@gmail.com>
2025-12-09 22:18:04 +0200

    feat: created entities with relations

--- src/transaction/entities/transaction.entity.ts
+++ src/transaction/entities/transaction.entity.ts
@@ -1,1 +1,40 @@
-export class Transaction {}
+import { Category } from 'src/category/entities/category.entity';
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('transactions')
+export class Transaction {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  type: string;
+
+  @Column()
+  amount: number;
+
+  @ManyToOne(() => User, (user) => user.transactions)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @OneToMany(() => Category, (category) => category.transaction)
+  categories: Category[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,40 @@
-export class User {}
+import { Category } from 'src/category/entities/category.entity';
+import {Transaction} from 'src/transaction/entities/transaction.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+@Unique(['email'])
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @OneToMany(() => Category, (category) => category.user, {
+    onDelete: 'CASCADE',
+  })
+  categories: Category[];
+
+  @OneToMany(() => Transaction, (transaction) => transaction.user, {
+    onDelete: 'CASCADE',
+  })
+  transactions: Transaction[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
