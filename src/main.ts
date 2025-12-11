c9cf74463b3ade0ff5696812bc95be4ea9c548cd
serii <seriiburduja@gmail.com>
2025-12-11 22:24:27 +0200

    feat: install swagger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Transactions API')
+    .setDescription('API for managing money transactions')
+    .setVersion('1.0')
+    .addTag('transactions')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // удаляет поля, которых нет в DTO
