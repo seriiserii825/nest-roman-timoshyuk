c9df7d176ed78e018c3e0416555f4b68dfb4de5a
serii <seriiburduja@gmail.com>
2025-12-11 23:24:17 +0200

    feat: need to finish with swagger

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,18 @@
+import {ApiProperty} from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'test@mail.com',
+    description: 'The email of the user',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'strongPassword123',
+    description: 'The password of the user (minimum 6 characters)',
+  })
   @IsString()
   @MinLength(6)
   password: string;
